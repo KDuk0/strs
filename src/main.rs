32f16eb903f4f99cdb48b5d335fd5e08cfcaf3f5
KDuk0 <125793699+KDuk0@users.noreply.github.com>
2023-06-18 13:16:36 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+//need this to turn string back into number
+use std::str::FromStr;
 
 fn main() {
-    //standard string
+    //standard string with default value
     let mut word = String::from("Hello world!");
-    println!("Digest: {word}");
+    println!("{word}");
+    let words = "New string".to_string();
+    println!("{words}");
     
     //adding word to string
     word.push_str(" What?");
@@ -28,5 +32,26 @@ fn main() {
     word2.push_str("Not hello");
     println!("{}", word2);
 
+    //str(string literal) is known at compile time and is stored on the stack
+    let word3:&str = "Static";
+    println!("{}", word3);
+
+    //converting number to string
+    let number:u8 = 5;
+    number.to_string();
+    println!("{}", number);
+    //converting back to number
+    let number1: Result<u8, _> = u8::from_str("5");
+    println!("{:?}", number1);
     
+    //replacing one string with another
+    let word4 = String::from("Hello, bro, Hello");
+    println!("{} {}", word4.replace("Hello", "Holla"), word4.len());
+    println!("{:?}", word4.to_uppercase());
+
+    //combine 2 strings with +
+    let s1 = "Word1".to_string();
+    let s2 = " Word2".to_string();
+    let s3 = s1.clone() + &s2;
+    println!("{}", s3);
 }
